df8c60fd3e8150f0ee1a388f72ab00a5d3fa3b70
Deadjim <deadjim@pm.me>
2021-10-27 14:49:51 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,6 @@
 echo "Run TTYD"
 echo "Password: $PASSWORD"
 
-if [ "$GH_REPO" ]
-then
-  echo "Github Repo: $GH_REPO"
-  git clone ${GH_REPO}
-  git config --global user.email "$USER_EMAIL"
-  git config --global user.name "$USER_NAME"
-fi
-
 if [ -z "$PASSWORD" ]
 then
 TTYD_PASS=
