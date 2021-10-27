591ad4432c74b7cf04d9a0ed79bc4b0f5f96b3a8
Deadjim <deadjim@pm.me>
2021-10-27 14:24:09 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-echo "Run Jupyter Lab & VSCode & TTYD"
+echo "Run TTYD"
 echo "Password: $PASSWORD"
 
 if [ "$GH_REPO" ]
@@ -8,26 +8,6 @@ then
   git config --global user.email "$USER_EMAIL"
   git config --global user.name "$USER_NAME"
 fi
-
-if [ "$IPYNB_FILE" ]
-then
-  wget -nc "${IPYNB_FILE}"
-fi
-chmod -R 555 /workspace
-
-# Do not change the port number 8000.
-jupyter notebook  --NotebookApp.token=$PASSWORD --ip=0.0.0.0 --port=8000 --allow-root &
-
-if [ -z "$PASSWORD" ]
-then
-AUTH=none
-else
-AUTH=password
-fi
-
-# Do not change the port number 8010.
-export PORT="8010"
-code-server /workspace/ --bind-addr=0.0.0.0:8010 --auth $AUTH &
 
 if [ -z "$PASSWORD" ]
 then
